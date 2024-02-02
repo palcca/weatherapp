c0daae550083901ac4c3cd319c56be57811d868b
palcca <dgabor90@gmail.com>
2024-02-02 12:16:59 +0100

    shorter code for fetching and seting data

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-async function getWeatherData(city) {
-  const response = await fetch(
-    "https://api.weatherbit.io/v2.0/current?city=" +
-      city +
-      "&lang=hu&key=7769188b1d5c4c3991d23168442d44ec",
-    { mode: "cors" }
-  );
-  const Data = await response.json();
-  return Data;
-}
-const searchBtn = document.querySelector("#searchBtn")
-searchBtn.addEventListener("click", ()=>{
+
+document.querySelector("#searchBtn").addEventListener("click", ()=>{
     weather((document.querySelector("#cityInput").value))
 });
 
 let weather = async (city) => {
-  const Data = await getWeatherData(city);
+  const response = await fetch(
+        "https://api.weatherbit.io/v2.0/current?city=" +
+          city +
+          "&lang=hu&key=7769188b1d5c4c3991d23168442d44ec",
+        { mode: "cors" }
+  );
+  const Data = await response.json();
   document.querySelector("#location").textContent = Data.data[0].city_name+", " + Data.data[0].country_code; 
   document.querySelector("#temp").textContent = "Hömérséklet: " + Data.data[0].temp + " \u2103";
   document.querySelector("#winds").textContent = "Szél: " + (Math.floor(Number(Data.data[0].wind_spd)*3.6)) + "km/h, " + "Irány: " + Data.data[0].wind_cdir;
